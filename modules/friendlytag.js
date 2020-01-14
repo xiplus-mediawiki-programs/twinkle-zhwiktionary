xiplus-mediawiki-programs/twinkle-global

Unify twinkleconfig and friendlyconfig
https://github.com/azatoth/twinkle/commit/6b353546df867883af7f637a59e40b8d378c49e3

modules/friendlytag.js
@@ -10,7 +10,6 @@
  * Mode of invocation:     Tab ("Tag")
  * Active on:              Existing articles and drafts; file pages with a corresponding file
  *                         which is local (not on Commons); all redirects
- * Config directives in:   FriendlyConfig
  */
 
 TwinkleGlobal.tag = function friendlytag() {
@@ -69,8 +68,8 @@ TwinkleGlobal.tag.callback = function friendlytagCallback() {
 				tooltip: 'You can change the default view order in your Twinkle preferences (WP:TWPREFS).',
 				event: TwinkleGlobal.tag.updateSortOrder,
 				list: [
-					{ type: 'option', value: 'cat', label: 'By categories', selected: TwinkleGlobal.getFriendlyPref('tagArticleSortOrder') === 'cat' },
-					{ type: 'option', value: 'alpha', label: 'In alphabetical order', selected: TwinkleGlobal.getFriendlyPref('tagArticleSortOrder') === 'alpha' }
+					{ type: 'option', value: 'cat', label: 'By categories', selected: TwinkleGlobal.getPref('tagArticleSortOrder') === 'cat' },
+					{ type: 'option', value: 'alpha', label: 'In alphabetical order', selected: TwinkleGlobal.getPref('tagArticleSortOrder') === 'alpha' }
 				]
 			});
 
@@ -142,7 +141,7 @@ TwinkleGlobal.tag.callback = function friendlytagCallback() {
 						value: 'group',
 						name: 'group',
 						tooltip: 'If applying two or more templates supported by {{multiple issues}} and this box is checked, all supported templates will be grouped inside a {{multiple issues}} template.',
-						checked: TwinkleGlobal.getFriendlyPref('groupByDefault')
+						checked: TwinkleGlobal.getPref('groupByDefault')
 					}
 				]
 			}
@@ -168,9 +167,9 @@ TwinkleGlobal.tag.callback = function friendlytagCallback() {
 			form.append({ type: 'header', label: 'Replacement tags' });
 			form.append({ type: 'checkbox', name: 'fileTags', list: TwinkleGlobal.tag.file.replacementList });
 
-			if (TwinkleGlobal.getFriendlyPref('customFileTagList').length) {
+			if (TwinkleGlobal.getPref('customFileTagList').length) {
 				form.append({ type: 'header', label: 'Custom tags' });
-				form.append({ type: 'checkbox', name: 'fileTags', list: TwinkleGlobal.getFriendlyPref('customFileTagList') });
+				form.append({ type: 'checkbox', name: 'fileTags', list: TwinkleGlobal.getPref('customFileTagList') });
 			}
 			break;
 
@@ -186,9 +185,9 @@ TwinkleGlobal.tag.callback = function friendlytagCallback() {
 			form.append({ type: 'header', label: 'Miscellaneous and administrative redirect templates' });
 			form.append({ type: 'checkbox', name: 'redirectTags', list: TwinkleGlobal.tag.administrativeList });
 
-			if (TwinkleGlobal.getFriendlyPref('customRedirectTagList').length) {
+			if (TwinkleGlobal.getPref('customRedirectTagList').length) {
 				form.append({ type: 'header', label: 'Custom tags' });
-				form.append({ type: 'checkbox', name: 'redirectTags', list: TwinkleGlobal.getFriendlyPref('customRedirectTagList') });
+				form.append({ type: 'checkbox', name: 'redirectTags', list: TwinkleGlobal.getPref('customRedirectTagList') });
 			}
 			break;
 
@@ -604,10 +603,10 @@ TwinkleGlobal.tag.updateSortOrder = function(e) {
 	}
 
 	// append any custom tags
-	if (TwinkleGlobal.getFriendlyPref('customTagList').length) {
+	if (TwinkleGlobal.getPref('customTagList').length) {
 		container.append({ type: 'header', label: 'Custom tags' });
 		container.append({ type: 'checkbox', name: 'articleTags',
-			list: TwinkleGlobal.getFriendlyPref('customTagList').map(function(el) {
+			list: TwinkleGlobal.getPref('customTagList').map(function(el) {
 				el.checked = TwinkleGlobal.tag.checkedTags.indexOf(el.value) !== -1;
 				return el;
 			})
@@ -1254,8 +1253,8 @@ TwinkleGlobal.tag.callbacks = {
 
 			pageobj.setPageText(pageText);
 			pageobj.setEditSummary(summaryText + TwinkleGlobal.getPref('summaryAd'));
-			pageobj.setWatchlist(TwinkleGlobal.getFriendlyPref('watchTaggedPages'));
-			pageobj.setMinorEdit(TwinkleGlobal.getFriendlyPref('markTaggedPagesAsMinor'));
+			pageobj.setWatchlist(TwinkleGlobal.getPref('watchTaggedPages'));
+			pageobj.setMinorEdit(TwinkleGlobal.getPref('markTaggedPagesAsMinor'));
 			pageobj.setCreateOption('nocreate');
 			pageobj.save(function() {
 				// special functions for merge tags
@@ -1269,7 +1268,7 @@ TwinkleGlobal.tag.callbacks = {
 					talkpage.setEditSummary('Proposing to merge [[:' + params.nonDiscussArticle + ']] ' +
 						(params.mergeTag === 'Merge' ? 'with' : 'into') + ' [[:' + params.discussArticle + ']]' +
 						TwinkleGlobal.getPref('summaryAd'));
-					talkpage.setWatchlist(TwinkleGlobal.getFriendlyPref('watchMergeDiscussions'));
+					talkpage.setWatchlist(TwinkleGlobal.getPref('watchMergeDiscussions'));
 					talkpage.setCreateOption('recreate');
 					talkpage.append();
 				}
@@ -1848,8 +1847,8 @@ TwinkleGlobal.tag.callbacks = {
 
 		pageobj.setPageText(pageText);
 		pageobj.setEditSummary(summaryText + TwinkleGlobal.getPref('summaryAd'));
-		pageobj.setWatchlist(TwinkleGlobal.getFriendlyPref('watchTaggedPages'));
-		pageobj.setMinorEdit(TwinkleGlobal.getFriendlyPref('markTaggedPagesAsMinor'));
+		pageobj.setWatchlist(TwinkleGlobal.getPref('watchTaggedPages'));
+		pageobj.setMinorEdit(TwinkleGlobal.getPref('markTaggedPagesAsMinor'));
 		pageobj.setCreateOption('nocreate');
 		pageobj.save();
 
@@ -1949,8 +1948,8 @@ TwinkleGlobal.tag.callbacks = {
 
 		pageobj.setPageText(text);
 		pageobj.setEditSummary(summary.substring(0, summary.length - 2) + TwinkleGlobal.getPref('summaryAd'));
-		pageobj.setWatchlist(TwinkleGlobal.getFriendlyPref('watchTaggedPages'));
-		pageobj.setMinorEdit(TwinkleGlobal.getFriendlyPref('markTaggedPagesAsMinor'));
+		pageobj.setWatchlist(TwinkleGlobal.getPref('watchTaggedPages'));
+		pageobj.setMinorEdit(TwinkleGlobal.getPref('markTaggedPagesAsMinor'));
 		pageobj.setCreateOption('nocreate');
 		pageobj.save();
 

modules/friendlytalkback.js
@@ -405,12 +405,13 @@ var callback_evaluate = function(e) {
 		}
 
 	} else if (tbtarget === 'mail') {
-		text = '\n\n==' + TwinkleGlobal.getFriendlyPref('mailHeading') + "==\n{{you've got mail|subject=";
-		text += section + '|ts=~~~~~}}';
+		text +=
+			'==' + TwinkleGlobal.getPref('mailHeading') + '==\n' +
+			"{{You've got mail|subject=" + section + '|ts=~~~~~}}';
 
 		if (message) {
 			text += '\n' + message.trim() + '  ~~~~';
-		} else if (TwinkleGlobal.getFriendlyPref('insertTalkbackSignature')) {
+		} else if (TwinkleGlobal.getPref('insertTalkbackSignature')) {
 			text += '\n~~~~';
 		}
 
@@ -427,7 +428,7 @@ var callback_evaluate = function(e) {
 
 	} else {  // tbtarget one of mytalk, usertalk, other
 		// clean talkback heading: strip section header markers that were erroneously suggested in the documentation
-		text = '\n\n==' + TwinkleGlobal.getFriendlyPref('talkbackHeading').replace(/^\s*=+\s*(.*?)\s*=+$\s*/, '$1') + '==\n{{talkback|';
+		text = '\n\n==' + TwinkleGlobal.getPref('talkbackHeading').replace(/^\s*=+\s*(.*?)\s*=+$\s*/, '$1') + '==\n{{talkback|';
 		text += tbPageName;
 
 		if (section) {
@@ -438,7 +439,7 @@ var callback_evaluate = function(e) {
 
 		if (message) {
 			text += '\n' + message.trim() + ' ~~~~';
-		} else if (TwinkleGlobal.getFriendlyPref('insertTalkbackSignature')) {
+		} else if (TwinkleGlobal.getPref('insertTalkbackSignature')) {
 			text += '\n~~~~';
 		}
 
@@ -452,7 +453,7 @@ var callback_evaluate = function(e) {
 
 	talkpage.setAppendText(text);
 	talkpage.setCreateOption('recreate');
-	talkpage.setMinorEdit(TwinkleGlobal.getFriendlyPref('markTalkbackAsMinor'));
+	talkpage.setMinorEdit(TwinkleGlobal.getPref('markTalkbackAsMinor'));
 	talkpage.setFollowRedirect(true);
 	talkpage.append();
 };

twinkle.js
@@ -20,7 +20,7 @@
 
 /* global MorebitsGlobal */
 
-(function (window, document, $, undefined) { // Wrap with anonymous function
+(function (window, document, $) { // Wrap with anonymous function
 
 var TwinkleGlobal = {};
 window.TwinkleGlobal = TwinkleGlobal;  // allow global access
@@ -36,14 +36,14 @@ TwinkleGlobal.addInitCallback = function twinkleAddInitCallback(func) {
 
 TwinkleGlobal.defaultConfig = {};
 /**
- * Twinkle.defaultConfig.twinkle and Twinkle.defaultConfig.friendly
- *
- * This holds the default set of preferences used by Twinkle. (The |friendly| object holds preferences stored in the FriendlyConfig object.)
+ * This holds the default set of preferences used by Twinkle.
  * It is important that all new preferences added here, especially admin-only ones, are also added to
  * |Twinkle.config.sections| in twinkleconfig.js, so they are configurable via the Twinkle preferences panel.
  * For help on the actual preferences, see the comments in twinkleconfig.js.
+ *
+ * Formerly Twinkle.defaultConfig.twinkle and Twinkle.defaultConfig.friendly
  */
-TwinkleGlobal.defaultConfig.twinkle = {
+TwinkleGlobal.defaultConfig = {
 	// General
 	summaryAd: ' (TwinkleGlobal)',
 	deletionSummaryAd: ' (TwinkleGlobal)',
@@ -158,34 +158,10 @@ TwinkleGlobal.defaultConfig.twinkle = {
 	dataApi: mw.config.get('wgDBname') === 'wikidatawiki'
 		? new mw.Api()
 		: new mw.ForeignApi('https://www.wikidata.org/w/api.php'),
-	speedyTemplateItem: 'Q4847311'
-};
+	speedyTemplateItem: 'Q4847311',
 
-// now some skin dependent config.
-switch (mw.config.get('skin')) {
-	case 'vector':
-		TwinkleGlobal.defaultConfig.twinkle.portletArea = 'right-navigation';
-		TwinkleGlobal.defaultConfig.twinkle.portletId = 'p-twinkle';
-		TwinkleGlobal.defaultConfig.twinkle.portletName = 'TW';
-		TwinkleGlobal.defaultConfig.twinkle.portletType = 'menu';
-		TwinkleGlobal.defaultConfig.twinkle.portletNext = 'p-search';
-		break;
-	case 'timeless':
-		TwinkleGlobal.defaultConfig.twinkle.portletArea = '#page-tools .sidebar-inner';
-		TwinkleGlobal.defaultConfig.twinkle.portletId = 'p-twinkle';
-		TwinkleGlobal.defaultConfig.twinkle.portletName = 'Twinkle';
-		TwinkleGlobal.defaultConfig.twinkle.portletType = null;
-		TwinkleGlobal.defaultConfig.twinkle.portletNext = 'p-userpagetools';
-		break;
-	default:
-		TwinkleGlobal.defaultConfig.twinkle.portletArea = null;
-		TwinkleGlobal.defaultConfig.twinkle.portletId = 'p-cactions';
-		TwinkleGlobal.defaultConfig.twinkle.portletName = null;
-		TwinkleGlobal.defaultConfig.twinkle.portletType = null;
-		TwinkleGlobal.defaultConfig.twinkle.portletNext = null;
-}
+	// Formerly defaultConfig.friendly:
 
-TwinkleGlobal.defaultConfig.friendly = {
 	// Tag
 	groupByDefault: true,
 	watchTaggedPages: true,
@@ -220,38 +196,44 @@ TwinkleGlobal.defaultConfig.friendly = {
 	markSharedIPAsMinor: true
 };
 
+// now some skin dependent config.
+switch (mw.config.get('skin')) {
+	case 'vector':
+		TwinkleGlobal.defaultConfig.portletArea = 'right-navigation';
+		TwinkleGlobal.defaultConfig.portletId = 'p-twinkle';
+		TwinkleGlobal.defaultConfig.portletName = 'TW';
+		TwinkleGlobal.defaultConfig.portletType = 'menu';
+		TwinkleGlobal.defaultConfig.portletNext = 'p-search';
+		break;
+	case 'timeless':
+		TwinkleGlobal.defaultConfig.portletArea = '#page-tools .sidebar-inner';
+		TwinkleGlobal.defaultConfig.portletId = 'p-twinkle';
+		TwinkleGlobal.defaultConfig.portletName = 'Twinkle';
+		TwinkleGlobal.defaultConfig.portletType = null;
+		TwinkleGlobal.defaultConfig.portletNext = 'p-userpagetools';
+		break;
+	default:
+		TwinkleGlobal.defaultConfig.portletArea = null;
+		TwinkleGlobal.defaultConfig.portletId = 'p-cactions';
+		TwinkleGlobal.defaultConfig.portletName = null;
+		TwinkleGlobal.defaultConfig.portletType = null;
+		TwinkleGlobal.defaultConfig.portletNext = null;
+}
+
+
 TwinkleGlobal.getPref = function twinkleGetPref(name) {
-	var result;
-	if (typeof TwinkleGlobal.prefs === 'object' && typeof TwinkleGlobal.prefs.twinkle === 'object') {
-		// look in Twinkle.prefs (twinkleoptions.js)
-		result = TwinkleGlobal.prefs.twinkle[name];
-	} else if (typeof window.TwinkleConfig === 'object') {
-		// look in TwinkleConfig
-		result = window.TwinkleConfig[name];
+	if (typeof TwinkleGlobal.prefs === 'object' && TwinkleGlobal.prefs[name]) {
+		return TwinkleGlobal.prefs[name];
 	}
-
-	if (result === undefined) {
-		return TwinkleGlobal.defaultConfig.twinkle[name];
+	// Old preferences format, used before twinkleoptions.js was a thing
+	if (typeof window.TwinkleConfig === 'object' && window.TwinkleConfig[name]) {
+		return window.TwinkleConfig[name];
 	}
-	return result;
+	if (typeof window.FriendlyConfig === 'object' && window.FriendlyConfig[name]) {
+		return window.FriendlyConfig[name];
+	}
+	return TwinkleGlobal.defaultConfig[name];
 };
-
-TwinkleGlobal.getFriendlyPref = function twinkleGetFriendlyPref(name) {
-	var result;
-	if (typeof TwinkleGlobal.prefs === 'object' && typeof TwinkleGlobal.prefs.friendly === 'object') {
-		// look in Twinkle.prefs (twinkleoptions.js)
-		result = TwinkleGlobal.prefs.friendly[name];
-	} else if (typeof window.FriendlyConfig === 'object') {
-		// look in FriendlyConfig
-		result = window.FriendlyConfig[name];
-	}
-
-	if (result === undefined) {
-		return TwinkleGlobal.defaultConfig.friendly[name];
-	}
-	return result;
-};
-
 
 
 /**
@@ -433,6 +415,34 @@ var scriptpathbefore = '//meta.wikimedia.org/w/index.php?title=',
 mw.loader.getScript(scriptpathbefore + 'User:' + encodeURIComponent(mw.config.get('wgUserName')) + '/twinkleoptions.js' + scriptpathafter)
 	.fail(function () {
 		mw.notify('Could not load twinkleoptions.js');
+	})
+	.done(function (optionsText) {
+
+		// Quick pass if user has no options
+		if (optionsText === '') {
+			return;
+		}
+
+		// Twinkle options are basically a JSON object with some comments. Strip those:
+		optionsText = optionsText.replace(/(?:^(?:\/\/[^\n]*\n)*\n*|(?:\/\/[^\n]*(?:\n|$))*$)/g, '');
+
+		// First version of options had some boilerplate code to make it eval-able -- strip that too. This part may become obsolete down the line.
+		if (optionsText.lastIndexOf('window.Twinkle.prefs = ', 0) === 0) {
+			optionsText = optionsText.replace(/(?:^window.Twinkle.prefs = |;\n*$)/g, '');
+		}
+
+		try {
+			var options = JSON.parse(optionsText);
+			if (options) {
+				if (options.twinkle || options.friendly) { // Old preferences format
+					TwinkleGlobal.prefs = $.extend(options.twinkle, options.friendly);
+				} else {
+					TwinkleGlobal.prefs = options;
+				}
+			}
+		} catch (e) {
+			mw.notify('Could not parse twinkleoptions.js');
+		}
 	})
 	.always(function () {
 		$(TwinkleGlobal.load);

xiplus-mediawiki-programs/twinkle-zhwiktionary

modules/friendlytag.js
@@ -8,9 +8,8 @@
  *** friendlytag.js: Tag module
  ****************************************
  * Mode of invocation:     Tab ("Tag")
- * Active on:              Existing articles;
- *                         all redirects
- * Config directives in:   FriendlyConfig
+ * Active on:              Existing articles and drafts; file pages with a corresponding file
+ *                         which is local (not on Commons); all redirects
  */
 
 Twinkle.tag = function friendlytag() {
@@ -55,7 +54,7 @@ Twinkle.tag.callback = function friendlytagCallback() {
 					label: wgULS('标记页面为已巡查', '標記頁面為已巡查'),
 					value: 'patrolPage',
 					name: 'patrolPage',
-					checked: Twinkle.getFriendlyPref('markTaggedPagesAsPatrolled')
+					checked: Twinkle.getPref('markTaggedPagesAsPatrolled')
 				}
 			]
 		});
@@ -73,8 +72,8 @@ Twinkle.tag.callback = function friendlytagCallback() {
 				tooltip: wgULS('您可以在Twinkle参数设置（WP:TWPREFS）中更改此项。', '您可以在Twinkle偏好設定（WP:TWPREFS）中更改此項。'),
 				event: Twinkle.tag.updateSortOrder,
 				list: [
-					{ type: 'option', value: 'cat', label: wgULS('按类别', '按類別'), selected: Twinkle.getFriendlyPref('tagArticleSortOrder') === 'cat' },
-					{ type: 'option', value: 'alpha', label: '按字母', selected: Twinkle.getFriendlyPref('tagArticleSortOrder') === 'alpha' }
+					{ type: 'option', value: 'cat', label: wgULS('按类别', '按類別'), selected: Twinkle.getPref('tagArticleSortOrder') === 'cat' },
+					{ type: 'option', value: 'alpha', label: '按字母', selected: Twinkle.getPref('tagArticleSortOrder') === 'alpha' }
 				]
 			});
 
@@ -401,10 +400,10 @@ Twinkle.tag.updateSortOrder = function(e) {
 	}
 
 	// append any custom tags
-	if (Twinkle.getFriendlyPref('customTagList').length) {
+	if (Twinkle.getPref('customTagList').length) {
 		container.append({ type: 'header', label: wgULS('自定义模板', '自訂模板') });
 		var customcheckboxes = [];
-		$.each(Twinkle.getFriendlyPref('customTagList'), function(_, item) {
+		$.each(Twinkle.getPref('customTagList'), function(_, item) {
 			customcheckboxes.push(makeCheckbox(item.value, item.label));
 		});
 		container.append({
@@ -571,8 +570,8 @@ Twinkle.tag.callbacks = {
 			pageobj.setPageText(pageText);
 			pageobj.setEditSummary(summaryText + Twinkle.getPref('summaryAd'));
 			pageobj.setTags(Twinkle.getPref('revisionTags'));
-			pageobj.setWatchlist(Twinkle.getFriendlyPref('watchTaggedPages'));
-			pageobj.setMinorEdit(Twinkle.getFriendlyPref('markTaggedPagesAsMinor'));
+			pageobj.setWatchlist(Twinkle.getPref('watchTaggedPages'));
+			pageobj.setMinorEdit(Twinkle.getPref('markTaggedPagesAsMinor'));
 			pageobj.setCreateOption('nocreate');
 			pageobj.save(function() {
 				// special functions for merge tags
@@ -588,7 +587,7 @@ Twinkle.tag.callbacks = {
 						'與' + '[[' + params.discussArticle + ']]合併') +
 						Twinkle.getPref('summaryAd'));
 					talkpage.setTags(Twinkle.getPref('revisionTags'));
-					talkpage.setWatchlist(Twinkle.getFriendlyPref('watchMergeDiscussions'));
+					talkpage.setWatchlist(Twinkle.getPref('watchMergeDiscussions'));
 					talkpage.setCreateOption('recreate');
 					talkpage.append();
 				}
@@ -901,8 +900,8 @@ Twinkle.tag.callbacks = {
 		pageobj.setPageText(pageText);
 		pageobj.setEditSummary(summaryText + Twinkle.getPref('summaryAd'));
 		pageobj.setTags(Twinkle.getPref('revisionTags'));
-		pageobj.setWatchlist(Twinkle.getFriendlyPref('watchTaggedPages'));
-		pageobj.setMinorEdit(Twinkle.getFriendlyPref('markTaggedPagesAsMinor'));
+		pageobj.setWatchlist(Twinkle.getPref('watchTaggedPages'));
+		pageobj.setMinorEdit(Twinkle.getPref('markTaggedPagesAsMinor'));
 		pageobj.setCreateOption('nocreate');
 		pageobj.save();
 
@@ -1012,8 +1011,8 @@ Twinkle.tag.callbacks = {
 		pageobj.setPageText(text);
 		pageobj.setEditSummary(summary.substring(0, summary.length - 2) + Twinkle.getPref('summaryAd'));
 		pageobj.setTags(Twinkle.getPref('revisionTags'));
-		pageobj.setWatchlist(Twinkle.getFriendlyPref('watchTaggedPages'));
-		pageobj.setMinorEdit(Twinkle.getFriendlyPref('markTaggedPagesAsMinor'));
+		pageobj.setWatchlist(Twinkle.getPref('watchTaggedPages'));
+		pageobj.setMinorEdit(Twinkle.getPref('markTaggedPagesAsMinor'));
 		pageobj.setCreateOption('nocreate');
 		pageobj.save();
 

modules/friendlytalkback.js
@@ -272,7 +272,7 @@ var callback_evaluate = function(e) {
 	var text;
 	var title, content, editSummary;
 	if (tbtarget === 'mail') {
-		title = Twinkle.getFriendlyPref('mailHeading');
+		title = Twinkle.getPref('mailHeading');
 		content = "{{you've got mail|subject=" + section + '|ts=~~~~~}}';
 
 		text = '\n\n==' + title + '==\n' + content;
@@ -280,14 +280,14 @@ var callback_evaluate = function(e) {
 		if (message) {
 			content += '\n' + message.trim();
 			text += '\n' + message.trim() + '--~~~~';
-		} else if (Twinkle.getFriendlyPref('insertTalkbackSignature')) {
+		} else if (Twinkle.getPref('insertTalkbackSignature')) {
 			text += '\n~~~~';
 		}
 
 		editSummary = wgULS('通知：有新邮件', '通知：有新郵件') + Twinkle.getPref('summaryAd');
 	} else {  // tbtarget one of mytalk, usertalk, other
 		// clean talkback heading: strip section header markers that were erroneously suggested in the documentation
-		title = Twinkle.getFriendlyPref('talkbackHeading').replace(/^\s*=+\s*(.*?)\s*=+$\s*/, '$1');
+		title = Twinkle.getPref('talkbackHeading').replace(/^\s*=+\s*(.*?)\s*=+$\s*/, '$1');
 		content = '{{talkback|' + tbPageName;
 
 		if (section) {
@@ -300,7 +300,7 @@ var callback_evaluate = function(e) {
 		if (message) {
 			content += '\n' + message.trim();
 			text += '\n' + message.trim() + '--~~~~';
-		} else if (Twinkle.getFriendlyPref('insertTalkbackSignature')) {
+		} else if (Twinkle.getPref('insertTalkbackSignature')) {
 			text += '\n~~~~';
 		}
 
@@ -323,7 +323,7 @@ var callback_evaluate = function(e) {
 		talkpage.setTags(Twinkle.getPref('revisionTags'));
 		talkpage.setAppendText(text);
 		talkpage.setCreateOption('recreate');
-		talkpage.setMinorEdit(Twinkle.getFriendlyPref('markTalkbackAsMinor'));
+		talkpage.setMinorEdit(Twinkle.getPref('markTalkbackAsMinor'));
 		talkpage.setFollowRedirect(true);
 		talkpage.append();
 	});

twinkle.js
@@ -21,7 +21,7 @@
 
 /* global Morebits */
 
-(function (window, document, $, undefined) { // Wrap with anonymous function
+(function (window, document, $) { // Wrap with anonymous function
 
 // MediaWiki:Gadget-site-lib.js
 window.wgUXS = function (wg, hans, hant, cn, tw, hk, sg, zh, mo, my) {
@@ -60,14 +60,14 @@ Twinkle.addInitCallback = function twinkleAddInitCallback(func) {
 
 Twinkle.defaultConfig = {};
 /**
- * Twinkle.defaultConfig.twinkle and Twinkle.defaultConfig.friendly
- *
- * This holds the default set of preferences used by Twinkle. (The |friendly| object holds preferences stored in the FriendlyConfig object.)
+ * This holds the default set of preferences used by Twinkle.
  * It is important that all new preferences added here, especially admin-only ones, are also added to
  * |Twinkle.config.sections| in twinkleconfig.js, so they are configurable via the Twinkle preferences panel.
  * For help on the actual preferences, see the comments in twinkleconfig.js.
+ *
+ * Formerly Twinkle.defaultConfig.twinkle and Twinkle.defaultConfig.friendly
  */
-Twinkle.defaultConfig.twinkle = {
+Twinkle.defaultConfig = {
 	// General
 	summaryAd: '',
 	deletionSummaryAd: '',
@@ -139,34 +139,10 @@ Twinkle.defaultConfig.twinkle = {
 	revisionTags: 'Twinkle',
 	configPage: 'Wiktionary:Twinkle/参数设置',
 	projectNamespaceName: mw.config.get('wgFormattedNamespaces')[4],
-	sandboxPage: 'Wiktionary:沙盒'
-};
+	sandboxPage: 'Wiktionary:沙盒',
 
-// now some skin dependent config.
-switch (mw.config.get('skin')) {
-	case 'vector':
-		Twinkle.defaultConfig.twinkle.portletArea = 'right-navigation';
-		Twinkle.defaultConfig.twinkle.portletId = 'p-twinkle';
-		Twinkle.defaultConfig.twinkle.portletName = 'TW';
-		Twinkle.defaultConfig.twinkle.portletType = 'menu';
-		Twinkle.defaultConfig.twinkle.portletNext = 'p-search';
-		break;
-	case 'timeless':
-		Twinkle.defaultConfig.twinkle.portletArea = '#page-tools .sidebar-inner';
-		Twinkle.defaultConfig.twinkle.portletId = 'p-twinkle';
-		Twinkle.defaultConfig.twinkle.portletName = 'Twinkle';
-		Twinkle.defaultConfig.twinkle.portletType = null;
-		Twinkle.defaultConfig.twinkle.portletNext = 'p-userpagetools';
-		break;
-	default:
-		Twinkle.defaultConfig.twinkle.portletArea = null;
-		Twinkle.defaultConfig.twinkle.portletId = 'p-cactions';
-		Twinkle.defaultConfig.twinkle.portletName = null;
-		Twinkle.defaultConfig.twinkle.portletType = null;
-		Twinkle.defaultConfig.twinkle.portletNext = null;
-}
+	// Formerly defaultConfig.friendly:
 
-Twinkle.defaultConfig.friendly = {
 	// Tag
 	watchTaggedPages: false,
 	watchMergeDiscussions: false,
@@ -183,38 +159,49 @@ Twinkle.defaultConfig.friendly = {
 	markSharedIPAsMinor: true
 };
 
+// now some skin dependent config.
+switch (mw.config.get('skin')) {
+	case 'vector':
+		Twinkle.defaultConfig.portletArea = 'right-navigation';
+		Twinkle.defaultConfig.portletId = 'p-twinkle';
+		Twinkle.defaultConfig.portletName = 'TW';
+		Twinkle.defaultConfig.portletType = 'menu';
+		Twinkle.defaultConfig.portletNext = 'p-search';
+		break;
+	case 'timeless':
+		Twinkle.defaultConfig.portletArea = '#page-tools .sidebar-inner';
+		Twinkle.defaultConfig.portletId = 'p-twinkle';
+		Twinkle.defaultConfig.portletName = 'Twinkle';
+		Twinkle.defaultConfig.portletType = null;
+		Twinkle.defaultConfig.portletNext = 'p-userpagetools';
+		break;
+	default:
+		Twinkle.defaultConfig.portletArea = null;
+		Twinkle.defaultConfig.portletId = 'p-cactions';
+		Twinkle.defaultConfig.portletName = null;
+		Twinkle.defaultConfig.portletType = null;
+		Twinkle.defaultConfig.portletNext = null;
+}
+
+
 Twinkle.getPref = function twinkleGetPref(name) {
-	var result;
-	if (typeof Twinkle.prefs === 'object' && typeof Twinkle.prefs.twinkle === 'object') {
-		// look in Twinkle.prefs (twinkleoptions.js)
-		result = Twinkle.prefs.twinkle[name];
-	} else if (typeof window.TwinkleConfig === 'object') {
-		// look in TwinkleConfig
-		result = window.TwinkleConfig[name];
+	// Temporarily disable summaryAd
+	if ($.inArray(name, ['summaryAd', 'deletionSummaryAd', 'protectionSummaryAd', 'blockSummaryAd']) !== -1) {
+		return '';
 	}
 
-	if (result === undefined) {
-		return Twinkle.defaultConfig.twinkle[name];
+	if (typeof Twinkle.prefs === 'object' && Twinkle.prefs[name]) {
+		return Twinkle.prefs[name];
 	}
-	return result;
+	// Old preferences format, used before twinkleoptions.js was a thing
+	if (typeof window.TwinkleConfig === 'object' && window.TwinkleConfig[name]) {
+		return window.TwinkleConfig[name];
+	}
+	if (typeof window.FriendlyConfig === 'object' && window.FriendlyConfig[name]) {
+		return window.FriendlyConfig[name];
+	}
+	return Twinkle.defaultConfig[name];
 };
-
-Twinkle.getFriendlyPref = function twinkleGetFriendlyPref(name) {
-	var result;
-	if (typeof Twinkle.prefs === 'object' && typeof Twinkle.prefs.friendly === 'object') {
-		// look in Twinkle.prefs (twinkleoptions.js)
-		result = Twinkle.prefs.friendly[name];
-	} else if (typeof window.FriendlyConfig === 'object') {
-		// look in FriendlyConfig
-		result = window.FriendlyConfig[name];
-	}
-
-	if (result === undefined) {
-		return Twinkle.defaultConfig.friendly[name];
-	}
-	return result;
-};
-
 
 
 /**
@@ -415,21 +402,13 @@ $.ajax({
 		}
 
 		try {
-			var options = $.parseJSON(optionsText);
-
-			// Assuming that our options evolve, we will want to transform older versions:
-			// if ( options.optionsVersion === undefined ) {
-			// ...
-			// options.optionsVersion = 1;
-			// }
-			// if ( options.optionsVersion === 1 ) {
-			// ...
-			// options.optionsVersion = 2;
-			// }
-			// At the same time, twinkleconfig.js needs to be adapted to write a higher version number into the options.
-
+			var options = JSON.parse(optionsText);
 			if (options) {
-				Twinkle.prefs = options;
+				if (options.twinkle || options.friendly) { // Old preferences format
+					Twinkle.prefs = $.extend(options.twinkle, options.friendly);
+				} else {
+					Twinkle.prefs = options;
+				}
 			}
 		} catch (e) {
 			mw.util.jsMessage('未能解析twinkleoptions.js');