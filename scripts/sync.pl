df0e0501b51b8e342f43dde4cb31ccd55e26af42
xiplus <huangxuanyuxiplus@gmail.com>
2021-09-24 11:16:46 +0800

    Update sync.pl

--- scripts/sync.pl
+++ scripts/sync.pl
@@ -428,12 +428,9 @@ twinkle.js
   twinkle.css
   morebits.js
   morebits.css
-  lib/select2.min.js
-  lib/select2.min.css
   modules/twinkleconfig.js
   modules/twinklearv.js
   modules/twinklebatchdelete.js
-  modules/twinklebatchundelete.js
   modules/twinkleblock.js
   modules/twinkleclose.js
   modules/twinklecopyvio.js
@@ -442,11 +439,9 @@ twinkle.js
   modules/twinkleimage.js
   modules/twinkleprotect.js
   modules/twinklespeedy.js
-  modules/twinklestub.js
   modules/twinkleunlink.js
   modules/twinklewarn.js
   modules/twinklexfd.js
   modules/friendlyshared.js
   modules/friendlytag.js
   modules/friendlytalkback.js
-  modules/friendlywelcome.js
